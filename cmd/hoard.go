1e0a311e0d012948d719adce3ef4e3ca3d415aa2
James Fennell <jamespfennell@gmail.com>
2021-02-07 15:47:42 -0500

    YAML config, POC

--- cmd/hoard.go
+++ cmd/hoard.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/jamespfennell/hoard"
 	"github.com/jamespfennell/hoard/config"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 )
+
+var configLocation = flag.String("config_file", "hoard.yml", "help")
 
 func main() {
 
@@ -22,24 +27,43 @@ func main() {
 		<-sigC
 		close(interruptC)
 	}()
-	c := config.Config{
-		Feeds: []config.Feed{
-			{
-				ID:          "PATH1",
-				Prefix:      "PATH1_",
-				Postfix:     ".gtfsrt",
-				URL:         "https://path.transitdata.nyc/gtfsrt",
-				Periodicity: 500 * time.Millisecond,
-			},
-			{
-				ID:          "PATH2",
-				Prefix:      "PATH2_",
-				Postfix:     ".gtfsrt",
-				URL:         "https://path.transitdata.nyc/gtfsrt",
-				Periodicity: 600 * time.Millisecond,
-			},
-		},
-	}
-	hoard.RunServer(c, "tmp", 10000, interruptC)
 
+	flag.Parse()
+	b, err := ioutil.ReadFile(*configLocation)
+	if err != nil {
+		fmt.Println("Could not read config file", err)
+
+		os.Exit(1)
+	}
+	fmt.Println(string(b))
+
+	var c2 config.Config
+	err = yaml.Unmarshal(b, &c2)
+	if err != nil {
+		fmt.Println("Could not read config file", err)
+		os.Exit(1)
+	}
+	fmt.Println(c2)
+
+	/*
+		c := config.Config{
+			Feeds: []config.Feed{
+				{
+					ID:          "PATH1",
+					Prefix:      "PATH1_",
+					Postfix:     ".gtfsrt",
+					URL:         "https://path.transitdata.nyc/gtfsrt",
+					Periodicity: 500 * time.Millisecond,
+				},
+				{
+					ID:          "PATH2",
+					Prefix:      "PATH2_",
+					Postfix:     ".gtfsrt",
+					URL:         "https://path.transitdata.nyc/gtfsrt",
+					Periodicity: 600 * time.Millisecond,
+				},
+			},
+		}
+	*/
+	hoard.RunServer(c2, "tmp", 10000, interruptC)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jamespfennell/hoard
 
 go 1.14
+
+require gopkg.in/yaml.v2 v2.4.0
