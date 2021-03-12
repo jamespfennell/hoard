e8ac076d64f4ad535a75699f00936f8fdfb933c5
James Fennell <jamespfennell@gmail.com>
2021-03-11 19:19:40 -0500

    Get rid of storage util.go file

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -225,3 +225,55 @@ type DStore interface {
 
 	Delete(dFile DFile) error
 }
+
+type CopyResult struct {
+	DFilesCopied []DFile
+	CopyErrors   []error
+	BytesCopied  int
+}
+
+func Copy(source ReadableDStore, target WritableDStore, hour hour.Hour) (CopyResult, error) {
+	result := CopyResult{}
+	dFiles, err := source.ListInHour(hour)
+	if err != nil {
+		return result, err
+	}
+	if len(dFiles) == 0 {
+		return result, nil
+	}
+	for _, dFile := range dFiles {
+		content, err := source.Get(dFile)
+		if err != nil {
+			if content != nil {
+				_ = content.Close()
+			}
+			result.CopyErrors = append(result.CopyErrors, err)
+			continue
+		}
+		reader := &countingReader{internalReader: content}
+		err = target.Store(dFile, reader)
+		if err != nil {
+			_ = content.Close()
+			result.CopyErrors = append(result.CopyErrors, err)
+			continue
+		}
+		if err := content.Close(); err != nil {
+			result.CopyErrors = append(result.CopyErrors, err)
+			continue
+		}
+		result.DFilesCopied = append(result.DFilesCopied, dFile)
+		result.BytesCopied += reader.count
+	}
+	return result, nil
+}
+
+type countingReader struct {
+	count          int
+	internalReader io.Reader
+}
+
+func (r *countingReader) Read(p []byte) (int, error) {
+	n, err := r.internalReader.Read(p)
+	r.count += n
+	return n, err
+}
