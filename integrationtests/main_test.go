bd944b00d54f3d57e711509f88e85c8683e687f7
James Fennell <jamespfennell@gmail.com>
2021-03-16 20:16:54 -0400

    Refactoring the existing test

--- integrationtests/main_test.go
+++ integrationtests/main_test.go
@@ -8,10 +8,8 @@ import (
 	"github.com/jamespfennell/hoard"
 	"github.com/jamespfennell/hoard/config"
 	"github.com/jamespfennell/hoard/integrationtests/external"
-	"github.com/jamespfennell/hoard/internal/util/testutil"
 	"io"
 	"os"
-	"path/filepath"
 	"reflect"
 	"testing"
 )
@@ -41,55 +39,35 @@ func TestMain(m *testing.M) {
 	os.Exit(code)
 }
 
+/*
 func Test_OceOperations(t *testing.T) {
 	minioServer1.EnsureLaunched()
 	minioServer2.EnsureLaunched()
 	t.Errorf("failed one")
 	return
-}
+}*/
 
 // TODO: support running the integration tests through the CLI
 //  in addition to the go package
 func Test_OnceOperations(t *testing.T) {
-
-
-	minioServer1.EnsureLaunched()
-	bucketName := minioServer1.NewBucket()
-
-	workspace, err := os.MkdirTemp("", "hoard-workspace-")
-	if err != nil {
-		t.Fatalf("Failed to create temp dir for test: %s\n", err)
-	}
-	defer func() {
-		err := os.RemoveAll(workspace)
-		if err != nil {
-			t.Errorf("Failed to clean up workspace: %s", err)
-		}
-	}()
+	workspace, err := external.NewFilesystem()
+	requireNilErr(t, err)
+	defer cleanUp(t, workspace)
 	fmt.Printf("Using %s as the Hoard workspace\n", workspace)
 
-	server, err := testutil.NewFeedServer()
-	if err != nil {
-		t.Fatalf("Failed to create feed server: %s\n", err)
-	}
-	defer func() {
-		if err := server.Shutdown(); err != nil {
-			t.Errorf("Failed to shut down feed server: %s", err)
-		}
-	}()
+	server, err := external.NewFeedServer()
+	requireNilErr(t, err)
+	defer cleanUp(t, server)
 	fmt.Println("Running feed server on port", server.Port())
 
 	c := &config.Config{
-		WorkspacePath: workspace,
+		WorkspacePath: workspace.String(),
 		Feeds: []config.Feed{
 			{
 				ID:      "feed1_",
 				Postfix: ".txt",
 				URL:     fmt.Sprintf("http://localhost:%d", server.Port()),
 			},
-		},
-		ObjectStorage: []config.ObjectStorage{
-			minioServer1.Config(bucketName),
 		},
 	}
 
@@ -117,23 +95,23 @@ func Test_OnceOperations(t *testing.T) {
 		}
 	}
 
-	var archivePaths []string
-	if err := filepath.Walk(filepath.Join(workspace, hoard.ArchivesSubDir), func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			t.Errorf("Failed to read file: %s", path)
-			return err
-		}
-		if info.IsDir() {
-			return nil
-		}
-		archivePaths = append(archivePaths, path)
-		return nil
-	}); err != nil {
-		t.Errorf("Error when walking the tree: %s\n", err)
+	archivePaths, err := workspace.SubDir(hoard.ArchivesSubDir).ListAllFiles()
+	if err != nil {
+		t.Errorf("Error when listing all archive files: %s\n", err)
 	}
 
+	allContent := getAllContents(t, archivePaths)
+
+	if !reflect.DeepEqual(allContent, server.Responses()) {
+		t.Errorf(
+			"Responses stored by Hoard: %v\nNot equal to responses sent by server: %v\n",
+			allContent, server.Responses())
+	}
+}
+
+func getAllContents(t *testing.T, paths []string) map[string]bool {
 	allContent := map[string]bool{}
-	for _, archivePath := range archivePaths {
+	for _, archivePath := range paths {
 		b, err := os.ReadFile(archivePath)
 		if err != nil {
 			t.Errorf("Failed to read file %s; %s\n", archivePath, err)
@@ -149,12 +127,7 @@ func Test_OnceOperations(t *testing.T) {
 			fmt.Println("Found content", content)
 		}
 	}
-
-	if !reflect.DeepEqual(allContent, server.Responses()) {
-		t.Errorf(
-			"Responses stored by Hoard: %v\nNot equal to responses sent by server: %v\n",
-			allContent, server.Responses())
-	}
+	return allContent
 }
 
 func extract(b []byte) ([]string, error) {
@@ -182,4 +155,16 @@ func extract(b []byte) ([]string, error) {
 		result = append(result, buffer.String())
 	}
 	return result, nil
+}
+
+func cleanUp(t *testing.T, c interface{ CleanUp() error }) {
+	if err := c.CleanUp(); err != nil {
+		t.Fatalf("Failed to clean up: %s", err)
+	}
+}
+
+func requireNilErr(t *testing.T, err error) {
+	if err != nil {
+		t.Fatalf("Error: %s", err)
+	}
 }
